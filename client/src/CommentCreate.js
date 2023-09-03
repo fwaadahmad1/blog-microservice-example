17af44985ef86ffe886d4b2f6aec5186e9e7f1c0
Fwaad Ahmad <68646332+fwaadahmad1@users.noreply.github.com>
2023-09-03 23:21:42 +0530

    [feat: client] added comment and post components

---
+++ client/src/CommentCreate.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function CommentCreate({ postId }) {
+  const [content, setcontent] = useState("");
+
+  async function onSubmit(event) {
+    event.preventDefault();
+
+    await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
+      content,
+    });
+
+    setcontent("");
+  }
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>New Comment</label>
+          <input
+            className="form-control"
+            value={content}
+            onChange={(e) => setcontent(e.target.value)}
+          />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
+}
